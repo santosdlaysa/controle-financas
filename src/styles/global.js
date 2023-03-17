351cc40e5f263542f843d9747f5d68da5aaadd4f
Laysa Diniz <santosdlaysa@gmail.com>
2023-03-17 16:31:04 -0400

    iniciando projeto

---
+++ src/styles/global.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+       
+        font-family: 'Poppins', sans-serif;
+        background-color: #f5f5f5;
+    }
+
+    `;
+
+    export default Global;
